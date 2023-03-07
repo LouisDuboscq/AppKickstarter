b3fa6f753ad765f0732e1f54f8f4e8c793c8d8bd
louis <duboscq.louis@gmail.com>
2023-03-07 17:55:11 +0100

    removes ssr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap"; 
-import netlify from '@astrojs/netlify/functions';
+import sitemap from "@astrojs/sitemap";  
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
@@ -14,7 +13,5 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
-  ],
-  output: 'server',
-  adapter: netlify(),
+  ], 
 });
