6240f48b001502bfac0cfd770f7547208473fda4
louis <duboscq.louis@gmail.com>
2023-05-17 22:47:01 +0200

    fix suffix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config.js";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
